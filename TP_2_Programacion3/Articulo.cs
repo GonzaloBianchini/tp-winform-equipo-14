4f194992572ef8884447d0a02d73722025999839
Andres Gonzalo Bianchini <gonzabianchini@gmail.com>
2024-04-16 00:43:30 -0300

    Se empieza la clase Articulo

---
+++ TP_2_Programacion3/Articulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_2_Programacion3
+{
+    class Articulo
+    {
+        int id;
+        string codigo;
+        string nombre;
+        string descripcion;
+        int idMarca;
+        int idCategoria;
+        float precio;
+
+        //to do
+
+        //List<Imagenes> listaImagenes;
+        //Categoria categoria;
+        //Marca marca;
+    }
+}
