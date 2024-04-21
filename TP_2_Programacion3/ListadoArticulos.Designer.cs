5cf7c39f6e2a44c8ea4e4a8794cf9c0531846202
Martin Ibañez <martinibanez.89@gmail.com>
2024-04-21 18:20:32 -0300

    Martin

--- TP_2_Programacion3/ListadoArticulos.Designer.cs
+++ TP_2_Programacion3/ListadoArticulos.Designer.cs
@@ -64,7 +64,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(988, 80);
+            this.dataGridView1.Size = new System.Drawing.Size(783, 80);
             this.dataGridView1.TabIndex = 1;
             // 
             // Codigo
@@ -113,7 +113,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1021, 442);
+            this.ClientSize = new System.Drawing.Size(873, 546);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonVolver);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
